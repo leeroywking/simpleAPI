9fc99f671290aba6b58c5d4c7a7e4903e2d8512f
leeroywking <leeroywking@gmail.com>
2019-11-12 13:25:07 -0800

    Minimal api stood up

--- routes.js
+++ routes.js
@@ -1,9 +1,18 @@
-'use strict'
-const simple = require('./handlers/simple')
-const configured = require('./handlers/configured')
+'use strict';
+const simple = require('./handlers/simple');
+const userObjJSON = require('./userObj');
 
-module.exports = function (app, opts) {
+module.exports = function(app, opts) {
   // Setup routes, middleware, and handlers
-  app.get('/', simple)
-  app.get('/configured', configured(opts))
-}
+  app.get('/', simple);
+
+  app.post('/personObject', personObjectRoute);
+  app.get('/personObject', personObjectRoute);
+};
+
+const personObjectRoute = (req, res) => {
+  console.log('START');
+  console.log(req);
+  console.log('END');
+  res.send(userObjJSON);
+};
